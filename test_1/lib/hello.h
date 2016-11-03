e4abcb0f7e7e23b31e3867bbb17fcc9caa42ded6
linengier <linengier@126.com>
2016-11-03 19:11:25 +0800

    my cmake test

---
+++ test_1/lib/hello.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+    > File Name: hello.h
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2016年11月02日 星期三 09时55分29秒
+ ************************************************************************/
+#ifndef CMAKE_TEST_HELLO_H
+#define CMAKE_TEST_HELLO_H
+extern int hello();
+#endif
+
